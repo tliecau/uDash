15d91d37cbc251d765aacb0f1e5b26760a6434df
wallie <tliecau@gmail.com>
2017-12-19 22:59:38 +0100

    Initial commit

---
+++ Services/LeaveAMessage/src/main/java/com/uDash/LeaveAMessage/LeaveAMessage.java
@@ -0,0 +1,22 @@
+package com.uDash.LeaveAMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan
+public class LeaveAMessage extends SpringBootServletInitializer {
+    public static void main(String[] args)  {
+        SpringApplication.run(LeaveAMessage.class, args);
+    }
+
+    @Override
+    protected final SpringApplicationBuilder configure (SpringApplicationBuilder application){
+        return application.sources(LeaveAMessage.class);
+    }
+}
